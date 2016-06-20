e4dc881de76a6e22a3a0342b6e113f5634e211b8
jpolensk <jan.polensky@b-tu.de>
2016-06-20 19:29:21 +0200

    20160620:1929 Gameloop läuft stabil TODO: Implementierung des Spielkonzepts

--- app/src/main/java/com/example/zhoosch/meintetrisspiel/ImSpielActivity.java
+++ app/src/main/java/com/example/zhoosch/meintetrisspiel/ImSpielActivity.java
@@ -13,7 +13,6 @@ import java.util.concurrent.Semaphore;
 // erster Test für die Versionierung
 public class ImSpielActivity extends AppCompatActivity implements Runnable {
 
-  private static final String DOOF_GELAUFEN = "000123";
   private CustomDrawableView mCanvas;
   private Thread mLocalGameThread = null;
 
@@ -37,48 +36,49 @@ public class ImSpielActivity extends AppCompatActivity implements Runnable {
 
   @Override
   public void run() {
-    // TODO: UND HIER STARTEN WIR! MIT EINEM TIMER!
-    // von hier: http://stackoverflow.com/questions/5274619/investigation-of-optimal-sleep-time-calculation-in-game-loop
-    final Semaphore mutexRefresh = new Semaphore(0);
-    final Semaphore mutexRefreshing = new Semaphore(1);
-    int refresh = 0;
+    try {
+      // TODO: UND HIER STARTEN WIR! MIT EINEM TIMER!
+      // von hier: http://stackoverflow.com/questions/5274619/investigation-of-optimal-sleep-time-calculation-in-game-loop
+      final Semaphore mutexRefresh = new Semaphore(0);
+      final Semaphore mutexRefreshing = new Semaphore(1);
+      int refresh = 0;
 
 
-    Timer timeRefresh = new Timer();
-    timeRefresh.scheduleAtFixedRate(new TimerTask() {
-      @Override
-      public void run() {
-        if (mutexRefreshing.tryAcquire()) {
-          mutexRefreshing.release();
-          mutexRefresh.release();
+      Timer timeRefresh = new Timer();
+      timeRefresh.scheduleAtFixedRate(new TimerTask() {
+        @Override
+        public void run() {
+          if (mutexRefreshing.tryAcquire()) {
+            mutexRefreshing.release();
+            mutexRefresh.release();
+          }
         }
-      }
-    }, 0, 1000 / 50); // 50 fps
-    /// hier blieben wir dann
-    Date startDate = new Date();
-    while (true) {
-      if (Thread.currentThread().isInterrupted()) {
-        Log.d("ACHTUNG", "Thread wird beendet!");
-        return;
-      }
-      try {
+      }, 0, 1000 / 50); // 50 fps
+      /// hier blieben wir dann
+      Date startDate = new Date();
+      while (true) {
+        if (Thread.currentThread().isInterrupted()) {
+          Log.d("ACHTUNG", "Thread wird beendet!");
+          return;
+        }
         mutexRefresh.acquire();
         mutexRefreshing.acquire();
-      } catch (InterruptedException ie) {
-        Log.e(DOOF_GELAUFEN, "run: gameloop", ie);
+
+        // refresh
+        refresh += 1;
+
+        if ((refresh = (refresh % 50)) == 0) {
+          Date endDate = new Date();
+          // folgende Ausgabe landet auf der logcat Konsole:
+          System.out.println(String.valueOf(50.0 * 1000 / (endDate.getTime() - startDate.getTime())) + " fps.");
+          startDate = new Date();
+        }
+        mutexRefreshing.release();
       }
-
-      // refresh
-      refresh += 1;
-
-
-      if ((refresh = (refresh % 50)) == 0) {
-        Date endDate = new Date();
-        // folgende Ausgabe landet auf der logcat Konsole:
-        System.out.println(String.valueOf(50.0 * 1000 / (endDate.getTime() - startDate.getTime())) + " fps.");
-        startDate = new Date();
-      }
-      mutexRefreshing.release();
+    } catch (InterruptedException ie) {
+      Log.e("DOOF_GELAUFEN", "run: gameloop", ie);
+      // TODO: SAVE STATS OF GAME
+      return;
     }
   }
 
@@ -87,6 +87,7 @@ public class ImSpielActivity extends AppCompatActivity implements Runnable {
     super.onStop();
     if (mLocalGameThread.isAlive() && !mLocalGameThread.isInterrupted()) {
       mLocalGameThread.interrupt();
+      mLocalGameThread = null;
       Log.d("lifecycle", "onStop Thread interrupted");
     }
     Log.d("LIFECYCLE", "onStop invoked");
@@ -97,6 +98,7 @@ public class ImSpielActivity extends AppCompatActivity implements Runnable {
     super.onPause();
     if (mLocalGameThread.isAlive() && !mLocalGameThread.isInterrupted()) {
       mLocalGameThread.interrupt();
+      mLocalGameThread = null;
       Log.d("lifecycle", "onPause Thread interrupted");
     }
     Log.d("LIFECYCLE", "onPause invoked");
@@ -106,7 +108,6 @@ public class ImSpielActivity extends AppCompatActivity implements Runnable {
   protected void onStart() {
     super.onStart();
     if (!mLocalGameThread.isAlive()) {
-      mLocalGameThread.run();
       Log.d("lifecycle", "onStart Thread gestartet");
     }
     Log.d("LIFECYCLE", "onStart invoked");
@@ -116,7 +117,6 @@ public class ImSpielActivity extends AppCompatActivity implements Runnable {
   protected void onResume() {
     super.onResume();
     if (!mLocalGameThread.isAlive()) {
-      mLocalGameThread.run();
       Log.d("lifecycle", "onResume Thread gestartet");
     }
     Log.d("LIFECYCLE", "onResume invoked");
@@ -126,8 +126,11 @@ public class ImSpielActivity extends AppCompatActivity implements Runnable {
   protected void onRestart() {
     super.onRestart();
     if (!mLocalGameThread.isAlive()) {
-      mLocalGameThread.run();
       Log.d("lifecycle", "onRestart Thread gestartet");
+      if (mLocalGameThread == null) {
+        mLocalGameThread = new Thread(this);
+        mLocalGameThread.start();
+      }
     }
     Log.d("LIFECYCLE", "onRestart invoked");
   }
